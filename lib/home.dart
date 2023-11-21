dcb4784878f0697f8ee17a8961171423fdc7b42a
chungvu <chung.vu@chudu24.com>
2023-11-21 16:46:54 +0700

    code

---
+++ lib/home.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState  extends State<HomePage> {
+  final List<String> selectStrategies = ['Select Strategy', 'Select Strategy2', 'Select Strategy3']; // List of dropdown items
+  final List<String> selectDepartments = ['Select Department', 'Select Department2', 'Select Department3']; // List of dropdown items
+  String selectedStrategy = 'Select Strategy'; // Default selected value
+  String selectedDepartment = 'Select Department'; // Default selected value
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        children: [
+          Container(
+            width: double.infinity, // Full width of the screen
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey), // Optional border
+              borderRadius: BorderRadius.circular(5), // Optional border radius
+            ),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: DropdownButton<String>(
+              isExpanded: true, // Fill the width of the container
+              value: selectedStrategy,
+              onChanged: (String? newValue) {
+                if (newValue != null) {
+                  setState(() {
+                    selectedStrategy = newValue;
+                  });
+                }
+              },
+              icon: Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(
+                      color: Colors.grey, // Border color
+                      width: 2, // Border width
+                    ),
+                  ), // Optional border
+                ),
+                padding: const EdgeInsets.only(left: 5),
+                child: const Icon(IconData(0xf13d, fontFamily: 'MaterialIcons')),
+              ),
+              items: selectStrategies.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+          Container(
+            width: double.infinity, // Full width of the screen
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey), // Optional border
+              borderRadius: BorderRadius.circular(5), // Optional border radius
+            ),
+            margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: DropdownButton<String>(
+              isExpanded: true, // Fill the width of the container
+              value: selectedDepartment,
+              onChanged: (String? newValue) {
+                if (newValue != null) {
+                  setState(() {
+                    selectedDepartment = newValue;
+                  });
+                }
+              },
+              icon: Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(
+                      color: Colors.grey, // Border color
+                      width: 2, // Border width
+                    ),
+                  ), // Optional border
+                ),
+                padding: const EdgeInsets.only(left: 5),
+                child: const Icon(IconData(0xf13d, fontFamily: 'MaterialIcons')),
+              ),
+              items: selectDepartments.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  // Widgets for each tab/screen
+  final List<Widget> _widgetOptions = <Widget>[
+    const Home(),
+    const Text('Search Page'),
+    const Text('Profile Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bottom Navigation Example'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue, // Color for selected item
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
